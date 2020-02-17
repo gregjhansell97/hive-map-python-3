e3b6042528e4c13c9ed97f27030866aee16846a0
gregjhansell97 <gregjhansell@gmail.com>
2020-02-17 15:59:24 -0500

    test: preliminary unit testing for transceiver
    basic transmits and receives tested on a single thread

---
+++ tests/local_transceiver.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from hmap.transceiver import Transceiver
+
+
+class LocalTransceiver(Transceiver):
+    """
+    Interacts with other transceivers on the same thread and process. 
+    LocalTransceiver is not thread-safe and is intended for testing purposes: 
+    spoofing up interactions between subscribers, publishers, and routers
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._connections = [] 
+
+    @staticmethod
+    def connect(nodes=[], **kwargs):
+        """
+        Connect two components that can use transceivers
+
+        Args:
+            u: component 1
+            v: component 2
+        """
+        connections = LocalTransceiver.get_connections(len(nodes), **kwargs)
+        for n, t in zip(nodes, connections):
+            n.use(t)
+
+    @staticmethod
+    def get_connections(size: int):
+        """
+        Creates a fully connected network of local transceivers
+
+        Args:
+            size: desired number of transceivers in fully-connected-network
+
+        Returns:
+            (list): list of LocalSocket instances all interconnected
+        """
+        connections = [LocalTransceiver() for _ in range(size)]
+        for t_i in connections:
+            for t_j in connections:
+                if t_i is not t_j:
+                    t_i.connect(t_j)
+        return connections
+
+    @property
+    def connections():
+        """
+        Tuple list of connections
+        """
+        return self._connections
+
+    def connect(self, connection: Transceiver):
+        """
+        Connects one local transceiver to another. The connection is one-way.
+
+        Args:
+            connection: transceiver connecting to
+        """
+        self._connections.append(connection)
+
+    def transmit(self, data: bytes):
+        """
+        Iterates through all connections and invokes their receive methods with 
+        a probability corresponding to the socket
+        """
+        for t in self._connections:
+            t.receive(data)
---
+++ tests/test_local_transceiver.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Unit tests for LocalSocket; the main target of these tests is AbstractSocket
+"""
+
+from collections import defaultdict
+import pytest
+
+from tests.local_transceiver import LocalTransceiver
+
+
+def get_callback():
+    """
+    Creates a callback instance that tracks invocations. This function was
+    created because a lot of tests create a callback!
+    
+    Returns (lambda t, d): callback that takes in a transceiver and data as
+        arguments
+    """
+    def cb(t: LocalTransceiver, data: bytes):
+        # callbacks for transceivers expects a transceiver instance and 
+        # raw-bytes being received
+        cb.log.append((t, data))
+    cb.log = [] # log tracks the data received and by which transceiver
+    return cb
+
+
+def test_initialization():
+    """
+    Ensures that local transceivers can be created
+    """
+    connections = LocalTransceiver.get_connections(10)
+
+def test_subscribe():
+    """
+    Ensures that _subscribe method of hmap.Transceiver works as expected
+    """
+    t = LocalTransceiver.get_connections(10)[0]
+    t._subscribe(None)
+
+def test_publish_subscribe_2_socket_network():
+    """
+    Create two transceivers and have one transmit data and the other receive 
+    that data
+    """
+    cb = get_callback()
+
+    connections = LocalTransceiver.get_connections(2)
+    connections[1]._subscribe(cb)
+    p_count = 10
+    # go through and publish data
+    for i in range(p_count):
+        connections[0].transmit(b"hello world")
+    assert cb.log == [(connections[1], b"hello world")] * 10
+
+def test_publish_subscribe_multiple_socket_network():
+    """
+    Creates several transceivers on the same network and verifies that successful
+    transmissions
+    """
+    cb = get_callback()
+
+    connections = LocalTransceiver.get_connections(10)
+    # each socket has their own callback
+    callbacks = [get_callback() for _ in connections]
+
+    # subscribe all sockets to their corresponding callbacks
+    for c, cb, i in zip(connections, callbacks, range(len(connections))):
+        c._subscribe(cb)
+
+    connections[1].transmit(b"hello world")
+    for c, cb in zip(connections, callbacks):
+        if c == connections[1]:
+            assert cb.log == []
+            cb.log.append((c, b"hello world")) # consistent throughout now
+        # useful for checking stuff later
+        assert cb.log == [(c, b"hello world")]
+
+    connections[5].transmit(b"lucky 5")
+    for c, cb in zip(connections, callbacks):
+        if c == connections[5]:
+            assert cb.log == [(c, b"hello world")]
+            cb.log.append((c, b"lucky 5"))
+        assert cb.log == [(c, b"hello world"), (c, b"lucky 5")]
